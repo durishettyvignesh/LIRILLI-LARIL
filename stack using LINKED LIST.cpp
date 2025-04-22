db085c698d6969dc227db69c70dad2fc035de822
durishettyvignesh <vigneshd0707@gmail.com>
2025-04-22 21:35:47 +0530

    WHAT ??
    yeah ... here there are some programs based on DSA and other LINKED LIST implementations 
    
    THIS REPOSITORY WILL BE CONTINUE TO BE EDITED AS THIS IN UNDER CONSTRUCTIONS ; }

---
+++ stack using LINKED LIST.cpp
@@ -0,0 +1,75 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct node{
+	int data;
+	struct node*link;
+}*top = NULL; 
+
+struct node*stack = NULL;
+
+void pop(int data)
+{
+    if(top==NULL)
+    {
+        printf("Stack Underflow");
+    }
+    else
+    {
+        free(top);
+    }
+    
+}
+
+void push(int data){
+	struct node* newnode;
+	newnode = (struct node*)malloc(4);
+	if(newnode == NULL ){
+		printf("bc stack me jaga nai hai");
+		exit(1);
+	}
+	newnode -> data = data ;
+	newnode -> link = NULL;
+	
+	newnode -> link = top;
+	top = newnode;
+}
+
+void display()
+{
+	struct node *temp;
+	temp = top;
+	while(temp)
+	{
+		printf("%d \n",temp->data);
+		temp = temp->link;
+	}
+	printf("\n");
+}
+
+int main()
+{
+	int choice , data;
+	while(1){
+		printf("enter choice\n");
+		printf("1.push\n2.display\n3.pop\n4.exit\n");
+		scanf("%d",&choice);
+		
+		switch(choice){
+			case 1 : printf("enter what data ?\n");
+			         scanf("%d",&data);
+					 push(data);
+					 break ;
+			case 2 : display(); break ;
+			
+			case 3 : pop(data);
+			break;
+			case 4 : exit(1);
+			default : printf("error");
+		break;
+		}
+
+	}
+	return 0;
+	
+}
