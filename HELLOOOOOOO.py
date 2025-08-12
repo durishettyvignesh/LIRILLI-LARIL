15d2240d11623e643f99e13c14b00164e1a64fa1
Vignesh Durishetty <vigneshd0707@gmail.com>
2025-08-12 14:27:43 +0530

    changes in heloooooo.py

--- HELLOOOOOOO.py
+++ HELLOOOOOOO.py
@@ -5,4 +5,9 @@ if a > 1e9:
 else:
     print("a is not greater than 1 billion")
 
-    
+# HELLOOOOOOO.py
+
+elif a < 1e9:
+    print("a is less than 1 billion")
+else:
+    print("a is equal to 1 billion") 
