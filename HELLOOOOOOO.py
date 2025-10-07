b3dd983c521da469615ba399ca68f08c6326ef06
durishettyvignesh <vigneshd0707@gmail.com>
2025-10-07 14:49:28 +0530

    remove redundant billion comparison logic

--- HELLOOOOOOO.py
+++ HELLOOOOOOO.py
@@ -1,13 +1,1 @@
-a = int(1e9)
-
-if a > 1e9:
-    print("a is greater than 1 billion")
-else:
-    print("a is not greater than 1 billion")
-
-# HELLOOOOOOO.py
-
-elif a < 1e9:
-    print("a is less than 1 billion")
-else:
-    print("a is equal to 1 billion") 
+import geck0lib
