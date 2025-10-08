7d973ade389d34a2ce2f3622274aa95da8dc3050
durishettyvignesh <vigneshd0707@gmail.com>
2025-10-08 09:52:30 +0530

    add initial implementation of file operations utility

---
+++ NULL TERMINATIONS/newfile.py/textfiletest.py
@@ -0,0 +1,10 @@
+file = open("1prog", "w")
+print(type(file))
+print(file)
+print(file.closed)
+
+
+"""
+A simple command-line utility to demonstrate safe file operations in Python.
+It includes functions to write, append, read, rename, and delete files safely.
+"""
